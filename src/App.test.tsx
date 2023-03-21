de7f61c2d2dcb206a1c350fd71f09511f9ecb0b1
Noemi <noemi.caggiano@gmail.com>
2023-03-21 15:18:22 +0000

    chore: ut

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import firebase, { initializeTestApp, assertSucceeds } from "@firebase/testing"
 
-test('renders singIn', () => {
-  render(<App />)
-  const signInButton = screen.getByTestId("signInWithGoogle")
-  expect(signInButton).toBeInTheDocument();
+const my_project_id = "typescript-chat-7885d"
+
+describe('chat app', () => {
+
+  test('renders singIn', () => {
+    render(<App />)
+    const signInButton = screen.getByTestId("signInWithGoogle")
+    expect(signInButton).toBeInTheDocument();
+  });
+
+  test('can read from database', async () => {
+    const db = initializeTestApp({projectId: my_project_id}).firestore();
+    const testDoc = db.collection("messages")
+    await assertSucceeds(testDoc.get())
+  })
+
 })
